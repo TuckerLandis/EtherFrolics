55450875fee09a1bd8cdeb74f056e6d92f76c6bc
Ben Hall <benjamhall@gmail.com>
2021-07-30 11:15:20 -0500

    import mui

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,8 +3,28 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import {useSelector} from 'react-redux';
+import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+// Material UI imports
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import Link from '@material-ui/core/Link';
 
 function Nav() {
+
+  // Material UI
+  const [drawer, setDrawer] = useState(false);
+
+  const toggleDrawer = () => {
+    setDrawer(!drawer);
+  };
+  // End Material UI
+
+  const history = useHistory();
   const user = useSelector((store) => store.user);
 
   let loginLinkData = {
@@ -20,7 +40,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">EtherFrolics</h2>
       </Link>
       <div>
         <Link className="navLink" to={loginLinkData.path}>
