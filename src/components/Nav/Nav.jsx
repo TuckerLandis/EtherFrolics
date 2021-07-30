817f2cb5859f0d26bc3ae61bc7a505535bd372dd
Ben Hall <benjamhall@gmail.com>
2021-07-30 15:19:46 -0500

    edits to navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import {useSelector} from 'react-redux';
@@ -14,7 +14,7 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import MenuIcon from '@material-ui/icons/Menu';
 import Drawer from '@material-ui/core/Drawer';
-// import Link from '@material-ui/core/Link';
+import Link from '@material-ui/core/Link';
 
 function Nav() {
 
