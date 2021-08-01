6efe08e8748698bc79c3580cac4dcb35076ab417
TuckerLandis <landistuckerc@gmail.com>
2021-07-31 19:33:14 -0500

    error handling added for image post

--- server/routes/image.router.js
+++ server/routes/image.router.js
@@ -8,21 +8,24 @@ const { uploadFile } = require('../s3')
 const multer = require('multer')
 const upload = multer({ dest: 'uploads/' })
 
-// endpoint for posting an image, succesfully posts to s3. this should likely be a transaction that both posts to s3, and then posts the images key to the DB
+// endpoint for posting an image, succesfully posts to s3. this should likely be a try catch finally that both posts to s3, and then posts the images key to the DB
 router.post('/', upload.single('image'), rejectUnauthenticated, async (req, res) => {
 
+    
     const file = req.file
     const info = req.body // .type?
-
     console.log('file info', file);
-
+    // uploads the image to s3
     const result = await uploadFile(file)
-
-    console.log('response from s3', result);
-
-
-
-    res.sendStatus(200)
+    .then(result => {
+        console.log('response from s3', result);
+    res.sendStatus(result)
+    })
+    .catch(error => {
+        console.log('error in image post', error);
+        
+    })
+    
 })
 
 
