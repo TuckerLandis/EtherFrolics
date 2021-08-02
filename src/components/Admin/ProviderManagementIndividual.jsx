a8333ac5c08a009c0aa9459950034e539d872da9
Emry Brisky <eabrisky@gmail.com>
2021-08-02 13:54:16 -0500

    committing

--- src/components/Admin/ProviderManagementIndividual.jsx
+++ src/components/Admin/ProviderManagementIndividual.jsx
@@ -46,7 +46,7 @@ function ProviderManagementIndividual() {
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const selectedProvider = useSelector(store => store.selectedProvider);
+    const provider = useSelector(store => store.selectedProvider);
 
     const params = useParams();
 
@@ -58,12 +58,12 @@ function ProviderManagementIndividual() {
     }, [params.id])
     // params.id in this array so that when it changes, the page refreshes^
 
-    console.log('Selected provider: ', selectedProvider);
+    console.log('Selected provider: ', provider);
 
     const soloProviderStatus = () => {
 
         // don't need ?
-        if (selectedProvider?.soloProvider == true) {
+        if (provider?.soloProvider == true) {
             return(
                 <p>Yes</p>
             )
@@ -75,22 +75,20 @@ function ProviderManagementIndividual() {
     } // end soloProviderStatus
 
     // dispatch uses useParams() to get id of selected provider (uses same action.type as ProviderManagementGeneral line 55)
-    // bring it back with useSelector, save to variable selectedProvider, line 48
-    // map over selectedProvider array (should be just the one provider object inside the array), line 82
-    // ^ since there's only ever one selected provider, is there a more efficient way to append their info to the DOM besides mapping over an array?
+    // bring it back with useSelector, save to variable provider, line 48
     // started to create sections to put provider info into, lines 90, 106, 109, 112, 115, 118
     // need to create edit button, work on functionality to conditionally render inputs and populate their values with preexisting data
     // need to set up dispatch for put routes to update provider object
     // need DISABLE functionality for admin - which db column will this change and how?
 
     // test concat for image path
-    const resumePath = `/api/image/ind/${selectedProvider[0]?.resumeKey}`
+    const resumePath = `/api/image/ind/${provider?.resumeKey}`
 
     return (
 
         <div>    
 
-                    <div key={selectedProvider?.provider_id}>
+                    <div key={provider?.provider_id}>
                         <div>
                             <h1>Provider General Information</h1>
                             <h2>Name: {provider?.firstName} {provider?.lastName}</h2>
@@ -113,36 +111,36 @@ function ProviderManagementIndividual() {
                         {/* works! can make this a light box, also only works atm if a provider has a resume image key, will bug otherwise, need to require the resume submission */}
                         <ImageViewer imagePath={resumePath} />
 
-                        <div>
+                        {/* <div>
                             <h1>Provider Credentials</h1>
-                            {provider.credential_array.map(credential => {
-                                return <p>{credential.credentialName}</p> // needs to be flesched out, but gets data back
+                            {provider?.credential_array.map(credential => {
+                                return <p>{credential?.credentialName}</p> // needs to be flesched out, but gets data back
                             })}
                         </div>
                         <div>
                             <h1>Provider Work History</h1>
-                            {provider.work_experience_array.map(workHistory => {
-                                return <p>{workHistory.workplace}</p> // needs to be flesched out, but gets data back
+                            {provider?.work_experience_array?.map(workHistory => {
+                                return <p>{workHistory?.workplace}</p> // needs to be flesched out, but gets data back
                             })}
                         </div>
                         <div>
                             <h1>Provider Mission History</h1>
-                            {provider.mission_experience_array.map(missionExp => {
-                                return <p>{missionExp.location}</p> // needs to be flesched out, but gets data back
+                            {provider?.mission_experience_array?.map(missionExp => {
+                                return <p>{missionExp?.location}</p> // needs to be flesched out, but gets data back
                             })}
                         </div>
                         <div>
                             <h1>Provider Education</h1>
-                            {provider.education_array.map(education => {
-                                return <p>{education.institution}</p> // needs to be flesched out, but gets data back
+                            {provider?.education_array?.map(education => {
+                                return <p>{education?.institution}</p> // needs to be flesched out, but gets data back
                             })}
                         </div>
                         <div>
                             <h1>Provider Insurance</h1>
-                            {provider.insurance_array.map(insurance => {
-                                return <p>{insurance.insuranceProvider}</p> // needs to be flesched out, but gets data back
+                            {provider?.insurance_array?.map(insurance => {
+                                return <p>{insurance?.insuranceProvider}</p> // needs to be flesched out, but gets data back
                             })}
-                        </div>
+                        </div> */}
                     </div>
 
         </div>
