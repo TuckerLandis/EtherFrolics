03b62b28458e1ffce3159d17cf786ad41c47df74
pthomas0309 <preston.thomas355@gmail.com>
2021-08-04 02:25:07 -0500

    consolidate accordian into one section with a clickable list

--- src/components/Provider/ProviderCredItem.jsx
+++ src/components/Provider/ProviderCredItem.jsx
@@ -3,9 +3,13 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { ListItem, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 
 function ProviderCredItem({ provider }) {
+
+    const handleClick = e => {
+        console.log(e.target.parentNode.parentNode.id)
+    }
 
     return (
         <div>
@@ -13,97 +17,21 @@ function ProviderCredItem({ provider }) {
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                 >
-                    <Typography variant="h6">Credential Name</Typography>
+                    <Typography variant="h6">Credentials</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Typography>
+                    <List component="nav">
                         {provider?.credential_array.map( (credential, i) => {
-                            return (<ListItem key={i}><ListItemText>{credential.credentialName}</ListItemText></ListItem>)
+                            console.log(credential);
+                            return (
+                                <ListItem key={i} id={credential.credential_id} button >
+                                    <ListItemText primary={`${credential.credentialName}`} onClick={handleClick} secondary={`Expiration: ${credential.dateExpiring}`} />
+                                </ListItem>
+                            )
                         })}
-                    </Typography>
+                    </List>
                 </AccordionDetails>
             </Accordion>
-
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                >
-                    <Typography variant="h6">Licensing Board</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                    {provider?.credential_array.map( (credential, i) => {
-                            return (<ListItem key={i}><ListItemText>{credential.licensingBoard}</ListItemText></ListItem>)
-                    })}
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                >
-                    <Typography variant="h6">License Number</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                    {provider?.credential_array.map( (credential, i) => {
-                            return (<ListItem key={i}><ListItemText>{credential.licenseNumber}</ListItemText></ListItem>)
-                    })}
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                >
-                    <Typography variant="h6">Issued Date</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                    {provider?.credential_array.map( (credential, i) => {
-                            return (<ListItem key={i}><ListItemText>{credential.dateInitial}</ListItemText></ListItem>)
-                    })}
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                >
-                    <Typography variant="h6">Date Renewed</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                    {provider?.credential_array.map( (credential, i) => {
-                            return (<ListItem key={i}><ListItemText>{credential.dateRenewed}</ListItemText></ListItem>)
-                    })}
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-            <Accordion>
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                >
-                    <Typography variant="h6">Expiration Date</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                    {provider?.credential_array.map( (credential, i) => {
-                            return (<ListItem key={i}><ListItemText>{credential.dateExpiring}</ListItemText></ListItem>)
-                    })}
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-
-
-
-
-
-
         </div>
     )
 }
