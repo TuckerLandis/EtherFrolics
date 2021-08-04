933671941dcef571130f7984d30a7916cbc587f7
Emry Brisky <eabrisky@gmail.com>
2021-08-04 16:51:35 -0500

    styling buttons on landing page

--- src/components/Admin/AdminLandingPage.jsx
+++ src/components/Admin/AdminLandingPage.jsx
@@ -15,6 +15,17 @@ const useStyles = makeStyles((theme) => ({
         '& > *': {
             margin: theme.spacing(1),
         },
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        margin: '5px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+    label: {
+        textTransform: 'capitalize',
     },
 }));
 
@@ -36,19 +47,41 @@ function AdminLandingPage() {
 
     return (
 
-        <div className={classes.root}>
+        <div>
 
-                    <div key={admin?.id}>
-                        <Typography
-                            variant='h3'
-                        >
-                            Welcome, {admin?.username}!
-                        </Typography>
-                    </div>
+            <div key={admin?.id}>
+                <Typography
+                    variant='h3'
+                    className="adminLandingTitle"
+                >
+                    Welcome, {admin?.username}!
+                </Typography>
+            </div>
 
-            <div></div>
-            <Button variant="contained" onClick={() => history.push('/providermgmt')}>Provider Management</Button>
-            <Button variant="contained" onClick={() => history.push('/missions')}>Mission Management</Button>
+            <div className="buttonCorral">
+                <Button
+                    className="adminBtn"
+                    variant="contained"
+                    classes={{
+                        root: classes.root, // class name, e.g. `classes-nesting-root-x`
+                        label: classes.label, // class name, e.g. `classes-nesting-label-x`
+                    }}
+                    onClick={() => history.push('/providermgmt')}
+                >
+                    Provider Management
+                </Button>
+                <Button
+                    className="adminBtn"
+                    variant="contained"
+                    classes={{
+                        root: classes.root, // class name, e.g. `classes-nesting-root-x`
+                        label: classes.label, // class name, e.g. `classes-nesting-label-x`
+                    }}
+                    onClick={() => history.push('/missions')}
+                >
+                    Mission Management
+                </Button>
+            </div>
 
         </div>
 
