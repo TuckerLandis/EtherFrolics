cd5fb593e32b09358656ff6c2fb367fa463aa2ff
Ben Hall <benjamhall@gmail.com>
2021-08-05 17:54:07 -0500

    mui button added to register page

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LoginForm from '../LoginForm/LoginForm';
 import { useHistory } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
-import { Typography } from '@material-ui/core';
+
 
 
 function LoginPage() {
