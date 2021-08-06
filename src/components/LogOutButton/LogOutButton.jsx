44f8995b7026305614af463d8675526b66b50b8b
pthomas0309 <preston.thomas355@gmail.com>
2021-08-06 07:05:32 -0500

    add resetProviderLanding method through logout dispatch

--- src/components/LogOutButton/LogOutButton.jsx
+++ src/components/LogOutButton/LogOutButton.jsx
@@ -11,13 +11,27 @@ function LogOutButton(props) {
     history.push('/')
   }
 
+  function resetProviderLanding() {
+
+    dispatch({
+      type: 'RESET_PROVIDER_LANDING_OBJ'
+    })
+  }
+
+  // function resetAdmin() {
+
+  //   dispatch({
+  //     type: 'RESET_ADMIN_OBJ'
+  //   })
+  // }
+
   return (
     <button
       // This button shows up in multiple locations and is styled differently
       // because it's styled differently depending on where it is used, the className
       // is passed to it from it's parents through React props
       className={props.className}
-      onClick={() => dispatch({ type: 'LOGOUT', pushToHome: pushToHome })}
+      onClick={() => dispatch({ type: 'LOGOUT', pushToHome: pushToHome, resetProviderLanding: resetProviderLanding})}
     >
       Log Out
     </button>
