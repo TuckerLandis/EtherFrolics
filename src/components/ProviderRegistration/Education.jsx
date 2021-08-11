eb306e483b5eb3b3d5c50f224b869c7f107683b8
pthomas0309 <preston.thomas355@gmail.com>
2021-08-11 15:56:22 -0500

    remove unused variables from education component

--- src/components/ProviderRegistration/Education.jsx
+++ src/components/ProviderRegistration/Education.jsx
@@ -2,7 +2,6 @@ import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { useHistory } from "react-router"
 import EducationMultiRow from "./FormComponents/EducationMultiRow";
-import ImageUploader from "../ImageComponents/ImageUploader";
 import { useSelector } from "react-redux";
 import RegistrationStepper from './Stepper'
 import { Typography } from "@material-ui/core";
@@ -17,18 +16,11 @@ function Education() {
     // state array on which education history sub components are rendered
     const [amountOfEducationHistories, setAmountOfEducationHistories] = useState([1])
 
-    // state variable to track if at least 1 section
-    // of education data has been submitted to the DB
-    const [educationSubmitted, setEducationSubmitted] = useState(false);
-
     // increases the amount of education history elements in the array above
     function addEducationHistoryItem() {
 
         setAmountOfEducationHistories(amountOfEducationHistories =>
             [...amountOfEducationHistories, amountOfEducationHistories.length + 1])
-
-        // education submitted set to true renders next button enabled
-        setEducationSubmitted(true);
 
     }
 
