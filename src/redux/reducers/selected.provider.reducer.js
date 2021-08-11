88b3f960fa7678b999e9e9d8d86ba6b982408f3c
pthomas0309 <preston.thomas355@gmail.com>
2021-08-11 16:10:50 -0500

    remove console logs for selected provider reducer

--- src/redux/reducers/selected.provider.reducer.js
+++ src/redux/reducers/selected.provider.reducer.js
@@ -1,8 +1,6 @@
 const selectedProvider = (state = {}, action) => {
     switch(action.type) {
         case 'SEND_PROVIDER':
-            console.log('individual provider reducer: ', action.payload);
-            
             return action.payload[0];
         default:
             return state;
